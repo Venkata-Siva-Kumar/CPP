9c1144c3b420d6d0c8d8a1fc09e7f680abb2b56f
Venkata-Siva-Kumar <mariyalasivakumar@gmail.com>
2025-05-07 11:05:35 +0530

    Update Floyd_Warshall.cpp

--- Floyd_Warshall.cpp
+++ Floyd_Warshall.cpp
@@ -1,101 +1,97 @@
-/* FLOYD WARSHALL */
+//FLOYD WARSHALL 
 #include<iostream>
+using namespace std;
 #define NIL 0
 #define max 10
-#define INF  1000
+#define INF 1000
+int n,w[max][max],d[max][max],pi[max][max];
 
-using namespace std;
-int n,w[max][max],d[max][max],p[max][max];
-
-void printMatrix(int a[][max])
+void print(int d[][max])
 {
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=n;j++)
-            cout<<a[i][j]<<"\t";
+        {
+            cout<<d[i][j]<<"\t";
+        }
         cout<<endl;
     }
 }
-
 void Floyd_Warshall(int w[][max])
 {
-    //Initialize Algorithm
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=n;j++)
         {
             d[i][j] = w[i][j];
-            if(i==j || w[i][j] == INF)
-                p[i][j] = NIL;
+            if(i==j || d[i][j] == INF)
+                    pi[i][j] = NIL;
             else
-                p[i][j] = i;
+                pi[i][j] = i;
         }
     }
+
     for(int k=1;k<=n;k++)
     {
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=n;j++)
             {
-                if(d[i][j] >d[i][k] + d[k][j])
+                if(d[i][j] > d[i][k]+d[k][j])
                 {
-                    d[i][j] = d[i][k] + d[k][j];
-                    p[i][j] = p[k][j];
-                }
-                else
-                {
-                    d[i][j] = d[i][j];
-                    p[i][j] = p[i][j];
+                    d[i][j] = d[i][k]+d[k][j];
+                    pi[i][j] = pi[k][j];
                 }
             }
         }
-        cout << endl << endl;
-        cout << "D" << k << " Matrix: " << endl;
-        printMatrix(d);
-    }
-}
-void printAllPairsShortestPath(int i, int j)
-{
-    if(i == j)
-        cout << i << " -> ";
-    else if(p[i][j] == NIL)
-        cout << "No path from " << i << " to " << j << " exists" << endl;
-    else 
-    {
-        printAllPairsShortestPath(i, p[i][j]);
-        cout << j << " -> ";
+
+        //cout<<"\n\n D"<<k<<" Matrix : "<<endl;
+        //print(d);
     }
 }
 
+void print_AllPairsShortestPath(int i,int j)
+{
+    if(i==j)
+        cout<<i<<"-->";
+    else if(pi[i][j] == NIL)
+        cout<<"No path exits from "<<i<<" to "<<j<<endl;
+    else
+    {
+        print_AllPairsShortestPath(i,pi[i][j]);
+        cout<<j<<"-->";
+    }
+}
 int main()
 {
-    cout<<"Enter the no.of Vertices : ";
+    cout<<"Enter the no. of Vertices : ";
     cin>>n;
 
     cout<<"***** Enter the Weighted Matrix *****"<<endl;
+
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=n;j++)
+        {
             cin>>w[i][j];
+        }
     }
 
     Floyd_Warshall(w);
-
     cout << "\n\nW matrix: " << endl;
-    printMatrix(w);
+    print(w);
    
     cout << endl;    
     cout << "D matrix: " << endl;
-    printMatrix(d);
+    print(d);
    
     cout << endl;    
-    cout << "P matrix: " << endl;
-    printMatrix(p);
-   
+    cout << "Pi matrix: " << endl;
+    print(pi);
     int from,to;
-    cout << "Enter the Shortest Path from - to : ";
-    cin>>from >>to;
-    printAllPairsShortestPath(from,to);
+    cout<<"Shortest path from - To : ";
+    cin>>from>>to;
+    print_AllPairsShortestPath(from,to);
 
     cout<<"\nCost from "<<from<<" to "<<to<<" is : ";
     cout<<d[from][to]<<endl;
